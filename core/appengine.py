06ce69c15401f2c31249b5253ff6e203860fcd7a
Jamison Kelly <jamison.m.kelly@gmail.com>
2025-09-29 21:39:06 -0400

    Add exception handeling for AppEngine.

--- core/appengine.py
+++ core/appengine.py
@@ -14,25 +14,40 @@ class AppEngine:
 
  
     def process_answer(self, cmd):
-        answer = round(float(cmd), 2)
-        if answer == self.correct_answer:
-            self.message = 'Correct!'
-        else:
-            self.message = f'Not Correct! (Expected ${self.correct_answer:.02f})\nYou answered ${answer:.02f}.'
-        self.correct_answer = None
+
+        try:
+            answer = round(float(cmd), 2)
+            if answer == self.correct_answer:
+                self.message = 'Correct!'
+            else:
+                self.message = f'Not Correct! (Expected ${self.correct_answer:.02f})\nYou answered ${answer:.02f}.'
+            self.correct_answer = None
+        except ValueError:
+            self.message = "The provided answer is not a valid number!"
+
 
 
     def process_add_item(self, cmd):
-        item_str = cmd[4:]
-        item_tuple = item_str.split(': ')
-        if len(item_tuple)==2:
-            name, price = item_tuple
-            item = Item(name, float(price))
-            self.items.add_item(item)
-            self.message = f'{item} added successfully.'
-        else:
-            self.message = f'Cannot add "{item_str}".\n'
-            self.message += 'Usage: add <item_name>: <item_price>'
+
+        try:
+            item_str = cmd[4:]
+            item_tuple = item_str.split(': ')
+            if len(item_tuple)==2:
+                name, price = item_tuple
+                item = Item(name, float(price))
+                self.items.add_item(item)
+                self.message = f'{item} added successfully.'
+            else:
+                self.message = f'Cannot add "{item_str}".\n'
+                self.message += 'Usage: add <item_name>: <item_price>'
+        except ValueError as e:
+            self.message = e
+        except InvalidItemNameError as e:
+            self.message = e
+        except DuplicateItemError as e:
+            self.message = e
+        except InvalidItemPriceError as e:
+            self.message = e
 
     def process_del_item(self, cmd):
         item_name = cmd[4: ]
