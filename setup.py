4aac09925719bb8a6293a918750e03e019ce4b9b
Jamison Kelly <jamison.m.kelly@gmail.com>
2025-09-30 17:01:43 -0400

    Add setup.py template based on instructions.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shoppinglistapp',
+      version='1.0.0',
+      description='Addition and Subtraction Practice App',
+      url='https://whatever.org/',
+      author='<FILL_IN_YOUR_FULL_NAME>',
+      author_email='<FILL_IN_YOUR_EMAIL>',
+      packages=['shoppinglistapp', 'shoppinglistapp.core'],
+      include_package_data=True,
+      zip_safe=False)
